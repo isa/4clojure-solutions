16ca5a9f4a6d37b3dce217ca0ff1458ecc633ef3
Isa Goksu <isa.goksu@gmail.com>
2011-11-10 16:39:01 -0700

    Solved third problem

---
+++ src/four_clojure_solutions/003_intro_to_strings.clj
@@ -0,0 +1,9 @@
+(ns four_clojure_solutions.003_intro_to_strings)
+
+; Intro to Strings
+; (= __ (.toUpperCase "hello world"))
+;
+; Fill in the blanks
+
+(defn solution [candidate]
+   (= candidate (.toUpperCase "hello world")))
---
+++ test/four_clojure_solutions/test_003_intro_to_strings.clj
@@ -0,0 +1,10 @@
+(ns four_clojure_solutions.test_003_intro_to_strings
+   (:use [midje.sweet])
+   (:use four_clojure_solutions.003_intro_to_strings))
+
+(fact "should return true when true given"
+  (solution "HELLO WORLD") => true)
+
+(fact "should return false for anything else"
+  (solution "something else") => false)
+
