aa8618e9773ae6a522c4b8687f34f3108fe6994f
Isa Goksu <isa.goksu@gmail.com>
2011-11-10 16:31:17 -0700

    Solved second problem

---
+++ src/four_clojure_solutions/002_simple_math.clj
@@ -0,0 +1,9 @@
+(ns four_clojure_solutions.002_simple_math)
+
+; Simple Math
+; (= (- 10 (* 2 3)) __)
+;
+; Fill in the blanks
+
+(defn solution [candidate]
+   (= (- 10 (* 2 3)) candidate))
---
+++ test/four_clojure_solutions/test_002_simple_math.clj
@@ -0,0 +1,10 @@
+(ns four_clojure_solutions.test_002_simple_math
+   (:use [midje.sweet])
+   (:use four_clojure_solutions.002_simple_math))
+
+(fact "should return true when true given"
+  (solution 4) => true)
+
+(fact "should return false for anything else"
+  (solution -1) => false)
+
